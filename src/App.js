25cb2924c72fd45e62aee42dff61b2b3b9c81ee4
rafaucoin <88778777+rafaucoin@users.noreply.github.com>
2022-03-25 08:58:51 +0100

    initial-commit

--- src/App.js
+++ src/App.js
@@ -5,24 +5,44 @@ import {
   Text,
   Link,
   VStack,
-  Code,
   Grid,
   theme,
+  FormControl,
+  Input,
+  FormHelperText,
+  FormLabel,
+  Button,
 } from '@chakra-ui/react';
+import { useEffect, useState, useRef } from 'react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState('');
+  // useEffect(() => {}, []);
+
+  const add = async e => {
+    e.preventDefault();
+    console.log('done');
+
+    const response = await axios.post(' http://localhost:3000/products', {
+      request: data,
+    });
+    console.log(response);
+  };
+
   return (
     <ChakraProvider theme={theme}>
       <Box textAlign="center" fontSize="xl">
         <Grid minH="100vh" p={3}>
           <ColorModeSwitcher justifySelf="flex-end" />
           <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
+            <form onSubmit={add}>
+              <FormLabel htmlFor="email">Email address</FormLabel>
+              <Input onChange={e => setData(e.target.value)} />
+              <FormHelperText>We'll never share your email.</FormHelperText>
+              <Button>submit</Button>
+            </form>
             <Link
               color="teal.500"
               href="https://chakra-ui.com"
