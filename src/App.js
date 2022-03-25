6f6ad69752a4bdac8f5850e51ebcaa4a75ca0830
Si-saber Sidali <dadisisaber@gmail.com>
2022-03-26 00:22:16 +0100

    Doc Card - choose profile - first page

--- src/App.js
+++ src/App.js
@@ -1,61 +1,10 @@
 import React from 'react';
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Grid,
-  theme,
-  FormControl,
-  Input,
-  FormHelperText,
-  FormLabel,
-  Button,
-} from '@chakra-ui/react';
-import { useEffect, useState, useRef } from 'react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import axios from 'axios';
+import DocCard from './components/cards/DocCard';
 
 function App() {
-  const [data, setData] = useState('');
-  // useEffect(() => {}, []);
-
-  const add = async e => {
-    e.preventDefault();
-    console.log('done');
-
-    const response = await axios.post(' http://localhost:3000/products', {
-      request: data,
-    });
-    console.log(response);
-  };
 
   return (
-    <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <form onSubmit={add}>
-              <FormLabel htmlFor="email">Email address</FormLabel>
-              <Input onChange={e => setData(e.target.value)} />
-              <FormHelperText>We'll never share your email.</FormHelperText>
-              <Button>submit</Button>
-            </form>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
-    </ChakraProvider>
+    <DocCard/>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import { ColorModeScript } from '@chakra-ui/react';
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(
   <StrictMode>
+    <ChakraProvider>
+
     <ColorModeScript />
     <App />
+    </ChakraProvider>
   </StrictMode>,
   document.getElementById('root')
 );
